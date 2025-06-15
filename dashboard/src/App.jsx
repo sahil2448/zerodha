9bcf855903d3bfc8889419d45a35014f2fa8d02c
Sahil Kamble <syk2448@gmail.com>
2025-06-15 16:37:40 +0530

    removed frontend subrepo

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/*" element={<Home />} />
+        <Route path="/*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
